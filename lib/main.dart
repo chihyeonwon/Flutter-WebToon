e221bd15320318087363411f4ddff84431406a6d
ylose <heungeob1003@gmail.com>
2023-01-28 12:04:57 +0900

    위젯들을 표시하는 home_screen.dart를 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: HomeScreen(),
     );
   }
 }
