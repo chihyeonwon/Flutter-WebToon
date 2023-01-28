8e21b82dca3b3a3c11dd6f15ea6e30481e164cb5
ylose <heungeob1003@gmail.com>
2023-01-28 12:56:28 +0900

    웹툰 url 수정

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,10 +1,10 @@
 import 'package:http/http.dart' as http; // namespace를 http로 지정
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev/";
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+    void getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today'); // 요청을 보낼 url
     final response = await http.get(url); // api에 get 방식으로 요청
     if(response.statusCode == 200) { // 200: 요청성공 
