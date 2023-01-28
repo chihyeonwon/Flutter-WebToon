e05d0db9ed2799f47fac92f95ea186f8a131859a
ylose <heungeob1003@gmail.com>
2023-01-28 12:46:58 +0900

    서버에 요청을 하고 응답 데이터를 콘솔에 뿌리는 getTodaysToons 메서드 생성

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,6 +4,13 @@ class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev/";
   final String today = "today";
 
-
-
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today'); // 요청을 보낼 url
+    final response = await http.get(url); // api에 get 방식으로 요청
+    if(response.statusCode == 200) { // 200: 요청성공 
+      print(response.body); // 응답의 데이터 부분을 콘솔에 출력
+      return; // 아무것도 반환하지 않음
+    }
+    throw Error(); // 아니라면 에러 발생
+  }
 }
