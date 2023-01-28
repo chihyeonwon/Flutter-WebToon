d8525dadcf7b155e67c56bc08a7d4837ad57b105
ylose <heungeob1003@gmail.com>
2023-01-28 12:34:27 +0900

    namespace를 http로 지정하고 라이브러리 사용

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http; // namespace를 http로 지정
+
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev/";
   final String today = "today";
