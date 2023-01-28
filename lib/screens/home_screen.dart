3de01662e77c9efe2a2aa5ed9189cc0168104aa7
ylose <heungeob1003@gmail.com>
2023-01-28 12:14:55 +0900

    AppBar 생성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor:Colors.white,
+      appBar:AppBar(
+        elevation:2, // 음영 테두리 조절
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title:const Center(
+          child:Text(
+            "오늘의 웹툰",
+            style:TextStyle(fontSize:24,),
+        ),
+        ),
+      ),
+    );
   }
 }
