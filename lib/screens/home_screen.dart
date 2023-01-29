95e782ec6e7d1b89b6c75ebce8fbe869aa9e6351
ylose <heungeob1003@gmail.com>
2023-01-29 12:08:46 +0900

    데이터를 가져오는 새로운 방법을 위해 stless로 다시 수정

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,30 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/webtoon_model.dart';
 import '../services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons(); // getTodaysToons 함수가 종료되기 까지 기다림
-    isLoading = false;
-    setState(() {
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
 
   @override
   Widget build(BuildContext context) {
