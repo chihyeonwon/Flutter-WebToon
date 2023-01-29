3f5ff7dd4dd4f04043b9a44ca507ea944113c0e9
ylose <heungeob1003@gmail.com>
2023-01-29 11:08:57 +0900

    webtoonModel json 클래스 리스트 생성

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,19 +1,22 @@
 import 'package:http/http.dart' as http; // namespace를 http로 지정
 import 'dart:convert'; // jsonDecode 메서드를 사용하기 위함
 
+import '../models/webtoon_model.dart';
+
 class ApiService {
+  List<WebtoonModel> webtoonInstances = [];
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-    void getTodaysToons() async {
+    Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today'); // 요청을 보낼 url
     final response = await http.get(url); // api에 get 방식으로 요청
     if(response.statusCode == 200) { // 200: 요청성공 
       final List<dynamic> webtoons = jsonDecode(response.body); // 응답의 데이터 부분을 콘솔에 출력
       for(var webtoon in webtoons) {
-        print(webtoon);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-      return; // 아무것도 반환하지 않음
+      return webtoonInstances;
     }
     throw Error(); // 아니라면 에러 발생
   }
