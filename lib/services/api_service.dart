588ec0c5a237520b3e1839d01ba1a72976a23436
ylose <heungeob1003@gmail.com>
2023-01-29 10:49:33 +0900

    응답의 문자열 데이터를 json형식으로 변환

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http; // namespace를 http로 지정
+import 'dart:convert'; // jsonDecode 메서드를 사용하기 위함
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -8,7 +9,10 @@ class ApiService {
     final url = Uri.parse('$baseUrl/$today'); // 요청을 보낼 url
     final response = await http.get(url); // api에 get 방식으로 요청
     if(response.statusCode == 200) { // 200: 요청성공 
-      print(response.body); // 응답의 데이터 부분을 콘솔에 출력
+      final List<dynamic> webtoons = jsonDecode(response.body); // 응답의 데이터 부분을 콘솔에 출력
+      for(var webtoon in webtoons) {
+        print(webtoon);
+      }
       return; // 아무것도 반환하지 않음
     }
     throw Error(); // 아니라면 에러 발생
