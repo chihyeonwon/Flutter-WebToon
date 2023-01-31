6d4ebd3ce79f6f7c2ffbf818d1231fd82e09dfcc
ylose <heungeob1003@gmail.com>
2023-01-31 10:33:49 +0900

    썸네일 이미지에 그림자 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -42,6 +42,13 @@ class HomeScreen extends StatelessWidget {
                            clipBehavior:Clip.hardEdge, // 자식의 부모 영역 침범을 제어
                            decoration: BoxDecoration(
                                borderRadius:BorderRadius.circular(20),
+                             boxShadow: [
+                               BoxShadow(
+                                 blurRadius:15, // 그림자의 범위 지정
+                                 offset: const Offset(10, 10), // 그림자의 위치 지정
+                                 color:Colors.black.withOpacity(0.5),
+                               )
+                             ]
                            ),
                            width: 250,
                              child: Image.network(webtoon.thumb)
