9f6dcb92a6add10c13d4f1a3ea1043838a91150a
ylose <heungeob1003@gmail.com>
2023-01-31 11:01:57 +0900

    어떤 웹툰인지 구분하기 위해서 title,thumb,id 정보를 생성자로 받아옴

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
 
   @override
   Widget build(BuildContext context) {
