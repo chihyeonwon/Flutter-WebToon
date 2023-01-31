5228313adc9cc4fe153f7a4efe4b4ddbefe27f5f
ylose <heungeob1003@gmail.com>
2023-01-31 11:27:36 +0900

    상세 페이지에 썸네일 이미지 코드 추가

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -24,6 +24,33 @@ class DetailScreen extends StatelessWidget {
           style:const TextStyle(fontSize:24,),
         ),
         ),
+      body: Column(
+        children: [
+          SizedBox(
+            height:50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  clipBehavior:Clip.hardEdge, // 자식의 부모 영역 침범을 제어
+                  decoration: BoxDecoration(
+                      borderRadius:BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius:15, // 그림자의 범위 지정
+                          offset: const Offset(10, 10), // 그림자의 위치 지정
+                          color:Colors.black.withOpacity(0.5),
+                        )
+                      ]
+                  ),
+                  width: 250,
+                  child: Image.network(thumb)
+              ),
+            ],
+          ),
+        ],
+      )
     );
   }
 }
