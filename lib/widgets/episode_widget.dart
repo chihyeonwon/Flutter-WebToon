7c13cfa0f00c7da9fe2f18bcc765aec811cec865
ylose <heungeob1003@gmail.com>
2023-02-04 12:38:32 +0900

    에피소드 버튼을 GestureDetector로 감싸기

--- lib/widgets/episode_widget.dart
+++ lib/widgets/episode_widget.dart
@@ -18,37 +18,40 @@ class Episode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin:const EdgeInsets.only(bottom:10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.green.shade400,
-          boxShadow: [
-            BoxShadow(
-              blurRadius:5,
-              offset:const Offset(5, 5),
-              color:Colors.black.withOpacity(0.2),
-            )
-          ]
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical:20,
-          horizontal:20,
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin:const EdgeInsets.only(bottom:10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.green.shade400,
+            boxShadow: [
+              BoxShadow(
+                blurRadius:5,
+                offset:const Offset(5, 5),
+                color:Colors.black.withOpacity(0.2),
+              )
+            ]
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:[
-            Text(episode.title,
-                style:const TextStyle(
-                  color:Colors.white,
-                  fontSize:16,
-                )
-            ),
-            const Icon(Icons.chevron_right_rounded,
-              color:Colors.white,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical:20,
+            horizontal:20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:[
+              Text(episode.title,
+                  style:const TextStyle(
+                    color:Colors.white,
+                    fontSize:16,
+                  )
+              ),
+              const Icon(Icons.chevron_right_rounded,
+                color:Colors.white,
+              ),
+            ],
+          ),
         ),
       ),
     );
