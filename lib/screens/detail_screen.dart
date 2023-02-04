a4086724f9a2983abdf3e6a9037fb100f5b00e33
ylose <heungeob1003@gmail.com>
2023-02-04 12:29:56 +0900

    에피소드 위젯을 episode_widget.dart 파일로 분리하고 import

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -4,6 +4,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 import '../models/webtoon_detail_model.dart';
 import '../models/webtoon_episode_model.dart';
 import '../services/api_service.dart';
+import '../widgets/episode_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
@@ -125,58 +126,6 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
         ),
       )
-    );
-  }
-}
-
-class Episode extends StatelessWidget {
-  const Episode({
-    Key? key,
-    required this.episode,
-  }) : super(key: key);
-
-  final WebtoonEpisodeModel episode;
-
-  // 에피소드 버튼을 탭했을 때 호출되는 함수
-  onButtonTap() async {
-    await launchUrlString("https://google.com");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin:const EdgeInsets.only(bottom:10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.green.shade400,
-        boxShadow: [
-          BoxShadow(
-            blurRadius:5,
-            offset:const Offset(5, 5),
-            color:Colors.black.withOpacity(0.2),
-          )
-        ]
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical:20,
-          horizontal:20,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:[
-            Text(episode.title,
-              style:const TextStyle(
-                color:Colors.white,
-                fontSize:16,
-              )
-            ),
-            const Icon(Icons.chevron_right_rounded,
-              color:Colors.white,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
