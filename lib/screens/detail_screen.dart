85d91157aa29244161f1111aa091c1ae36013f48
ylose <heungeob1003@gmail.com>
2023-02-04 12:25:44 +0900

    에피소드 버튼을 생성하는 부분을 Flutter Widget으로 추출하기

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -117,40 +117,7 @@ class _DetailScreenState extends State<DetailScreen> {
                     return Column(
                       children: [
                         for(var episode in snapshot.data!)
-                          Container(
-                            margin:const EdgeInsets.only(bottom:10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.green.shade400,
-                              boxShadow: [
-                                BoxShadow(
-                                  blurRadius:5,
-                                  offset:const Offset(5, 5),
-                                  color:Colors.black.withOpacity(0.2),
-                                )
-                              ]
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical:20,
-                                horizontal:20,
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children:[
-                                  Text(episode.title,
-                                    style:const TextStyle(
-                                      color:Colors.white,
-                                      fontSize:16,
-                                    )
-                                  ),
-                                  const Icon(Icons.chevron_right_rounded,
-                                    color:Colors.white,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )
+                          Episode(episode: episode)
                       ],
                     );
                   }
@@ -161,6 +128,53 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
         ),
       )
+    );
+  }
+}
+
+class Episode extends StatelessWidget {
+  const Episode({
+    Key? key,
+    required this.episode,
+  }) : super(key: key);
+
+  final WebtoonEpisodeModel episode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin:const EdgeInsets.only(bottom:10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.green.shade400,
+        boxShadow: [
+          BoxShadow(
+            blurRadius:5,
+            offset:const Offset(5, 5),
+            color:Colors.black.withOpacity(0.2),
+          )
+        ]
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical:20,
+          horizontal:20,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:[
+            Text(episode.title,
+              style:const TextStyle(
+                color:Colors.white,
+                fontSize:16,
+              )
+            ),
+            const Icon(Icons.chevron_right_rounded,
+              color:Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
